0e78504c27be346c7bbe45b747bb227ea3c6b1b0
Unknown <omar.maher.nassif@gmail.com>
2018-01-10 10:50:47 -0500

    Added first batch of input files

---
+++ ElasticPLaneStrain.m
@@ -0,0 +1,43 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% User input Start
+coor = [...
+    0 0
+    1 0
+    1 1
+    0 1];
+
+BC_T = {'x', 0, 'u', 0;...
+    'y', 0, 'v', 0};
+
+FORCE_Tx = {'x', 1, 'u', 1.0*19.0652152};
+
+NR_tol = 1e-9;%1e-11;
+max_iter = 100;%20
+n_steps = 5;
+eltype = 'Q4';
+plot = true;
+
+
+[nodes, elements, nen, ngp, numnp, numel, ndm, BC, FORCE] =...
+    Generate_mesh(eltype, coor, BC_T, FORCE_Tx, plot, 3, 3, 3);
+nummat = 1;
+material = 2; % PlaneStrain
+props = {...
+    'E', 200
+    'v', 0.25};
+
+ndof = 3;
+numeq = ndof*numnp;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% User input End
+
+%     options for props (substitute what's inside < > with a numeric value):
+%     material 1:
+%     props = {'E',<young's modulus>
+%              'v',<poisson's ratio>}
+
+%     material 2:
+%     props = {'E',<young's modulus>
+%              'v',<poisson's ratio>}
+
+%     material 3:
+%     props = {'mu'     ,  <mu's value>
+%              'lambda' ,  <lambda's value>};
